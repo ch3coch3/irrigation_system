1157f0e35a6a16f3aeebbd6923028c7dc50962f9
ch3coch3 <020611j87208x@gmail.com>
2021-06-12 15:55:30 +0800

    initial commit

---
+++ Debug/FreeRTOS/event_groups.d
@@ -0,0 +1,40 @@
+FreeRTOS/event_groups.o: ../FreeRTOS/event_groups.c \
+ C:/Users/02061/STM32CubeIDE/workspace_1.6.1/final\ project/FreeRTOS/include/FreeRTOS.h \
+ C:/Users/02061/STM32CubeIDE/workspace_1.6.1/final\ project/FreeRTOS/include/FreeRTOSConfig.h \
+ C:/Users/02061/STM32CubeIDE/workspace_1.6.1/final\ project/FreeRTOS/include/projdefs.h \
+ C:/Users/02061/STM32CubeIDE/workspace_1.6.1/final\ project/FreeRTOS/include/portable.h \
+ C:/Users/02061/STM32CubeIDE/workspace_1.6.1/final\ project/FreeRTOS/include/deprecated_definitions.h \
+ C:/Users/02061/STM32CubeIDE/workspace_1.6.1/final\ project/FreeRTOS/portable/ARM_CM4F/portmacro.h \
+ C:/Users/02061/STM32CubeIDE/workspace_1.6.1/final\ project/FreeRTOS/include/mpu_wrappers.h \
+ C:/Users/02061/STM32CubeIDE/workspace_1.6.1/final\ project/FreeRTOS/include/task.h \
+ C:/Users/02061/STM32CubeIDE/workspace_1.6.1/final\ project/FreeRTOS/include/list.h \
+ C:/Users/02061/STM32CubeIDE/workspace_1.6.1/final\ project/FreeRTOS/include/timers.h \
+ C:/Users/02061/STM32CubeIDE/workspace_1.6.1/final\ project/FreeRTOS/include/task.h \
+ C:/Users/02061/STM32CubeIDE/workspace_1.6.1/final\ project/FreeRTOS/include/event_groups.h \
+ C:/Users/02061/STM32CubeIDE/workspace_1.6.1/final\ project/FreeRTOS/include/timers.h
+
+C:/Users/02061/STM32CubeIDE/workspace_1.6.1/final\ project/FreeRTOS/include/FreeRTOS.h:
+
+C:/Users/02061/STM32CubeIDE/workspace_1.6.1/final\ project/FreeRTOS/include/FreeRTOSConfig.h:
+
+C:/Users/02061/STM32CubeIDE/workspace_1.6.1/final\ project/FreeRTOS/include/projdefs.h:
+
+C:/Users/02061/STM32CubeIDE/workspace_1.6.1/final\ project/FreeRTOS/include/portable.h:
+
+C:/Users/02061/STM32CubeIDE/workspace_1.6.1/final\ project/FreeRTOS/include/deprecated_definitions.h:
+
+C:/Users/02061/STM32CubeIDE/workspace_1.6.1/final\ project/FreeRTOS/portable/ARM_CM4F/portmacro.h:
+
+C:/Users/02061/STM32CubeIDE/workspace_1.6.1/final\ project/FreeRTOS/include/mpu_wrappers.h:
+
+C:/Users/02061/STM32CubeIDE/workspace_1.6.1/final\ project/FreeRTOS/include/task.h:
+
+C:/Users/02061/STM32CubeIDE/workspace_1.6.1/final\ project/FreeRTOS/include/list.h:
+
+C:/Users/02061/STM32CubeIDE/workspace_1.6.1/final\ project/FreeRTOS/include/timers.h:
+
+C:/Users/02061/STM32CubeIDE/workspace_1.6.1/final\ project/FreeRTOS/include/task.h:
+
+C:/Users/02061/STM32CubeIDE/workspace_1.6.1/final\ project/FreeRTOS/include/event_groups.h:
+
+C:/Users/02061/STM32CubeIDE/workspace_1.6.1/final\ project/FreeRTOS/include/timers.h:
---
+++ Debug/FreeRTOS/queue.d
@@ -0,0 +1,34 @@
+FreeRTOS/queue.o: ../FreeRTOS/queue.c \
+ C:/Users/02061/STM32CubeIDE/workspace_1.6.1/final\ project/FreeRTOS/include/FreeRTOS.h \
+ C:/Users/02061/STM32CubeIDE/workspace_1.6.1/final\ project/FreeRTOS/include/FreeRTOSConfig.h \
+ C:/Users/02061/STM32CubeIDE/workspace_1.6.1/final\ project/FreeRTOS/include/projdefs.h \
+ C:/Users/02061/STM32CubeIDE/workspace_1.6.1/final\ project/FreeRTOS/include/portable.h \
+ C:/Users/02061/STM32CubeIDE/workspace_1.6.1/final\ project/FreeRTOS/include/deprecated_definitions.h \
+ C:/Users/02061/STM32CubeIDE/workspace_1.6.1/final\ project/FreeRTOS/portable/ARM_CM4F/portmacro.h \
+ C:/Users/02061/STM32CubeIDE/workspace_1.6.1/final\ project/FreeRTOS/include/mpu_wrappers.h \
+ C:/Users/02061/STM32CubeIDE/workspace_1.6.1/final\ project/FreeRTOS/include/task.h \
+ C:/Users/02061/STM32CubeIDE/workspace_1.6.1/final\ project/FreeRTOS/include/list.h \
+ C:/Users/02061/STM32CubeIDE/workspace_1.6.1/final\ project/FreeRTOS/include/queue.h \
+ C:/Users/02061/STM32CubeIDE/workspace_1.6.1/final\ project/FreeRTOS/include/task.h
+
+C:/Users/02061/STM32CubeIDE/workspace_1.6.1/final\ project/FreeRTOS/include/FreeRTOS.h:
+
+C:/Users/02061/STM32CubeIDE/workspace_1.6.1/final\ project/FreeRTOS/include/FreeRTOSConfig.h:
+
+C:/Users/02061/STM32CubeIDE/workspace_1.6.1/final\ project/FreeRTOS/include/projdefs.h:
+
+C:/Users/02061/STM32CubeIDE/workspace_1.6.1/final\ project/FreeRTOS/include/portable.h:
+
+C:/Users/02061/STM32CubeIDE/workspace_1.6.1/final\ project/FreeRTOS/include/deprecated_definitions.h:
+
+C:/Users/02061/STM32CubeIDE/workspace_1.6.1/final\ project/FreeRTOS/portable/ARM_CM4F/portmacro.h:
+
+C:/Users/02061/STM32CubeIDE/workspace_1.6.1/final\ project/FreeRTOS/include/mpu_wrappers.h:
+
+C:/Users/02061/STM32CubeIDE/workspace_1.6.1/final\ project/FreeRTOS/include/task.h:
+
+C:/Users/02061/STM32CubeIDE/workspace_1.6.1/final\ project/FreeRTOS/include/list.h:
+
+C:/Users/02061/STM32CubeIDE/workspace_1.6.1/final\ project/FreeRTOS/include/queue.h:
+
+C:/Users/02061/STM32CubeIDE/workspace_1.6.1/final\ project/FreeRTOS/include/task.h:
